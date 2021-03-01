f356e7c5ff5cc68336a59bf56eef6bf86f3eb924
LZHD <lzhdfxq@163.com>
2021-03-01 21:56:43 +0800

    chore: first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Component } from "react";
+
+export default class PicturesViewer extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return <div>hello world</div>;
+  }
+}
